cbc2b92c0d62e63462523f7d419e3caca2c7af69
İzzet Aydin <izzetaydin@gmail.com>
2018-11-28 16:04:09 +0300

    Son commit

--- data/usr/local/bin/uyarlama.sh
+++
@@ -1,9 +0,0 @@
-awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd > /opt/uid.txt
-USR="$(cat /opt/uid.txt)"
-rm /opt/uid.txt
-
-cp -r /etc/skel/.java /home/$USR/.java
-cp -r /etc/skel/.java.policy /home/$USR/.java.policy
-cp -r /etc/skel/Masaüstü/*.desktop /home/$USR/Masaüstü/
-chown -R $USR:$USR /home/$USR/
-
